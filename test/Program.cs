e4521068d44b2c424c6e240f841ecf5d2fcfcb14
Olga Gladkaya <tarantasik@mail.ru>
2022-10-17 10:48:47 +0300

    added final task

--- test/Program.cs
+++ test/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter the size of the array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] CreateArray()
+{
+    string[] array = new string [size];
+    for (int index = 0; index < size; index++)
+    {
+        Console.Write($"Enter the string {index}: ");
+        array[index] = Convert.ToString(Console.ReadLine());
+    }
+    return array;
+}
+string[] arr = CreateArray();
+void CreateNewArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {   
+        if (arr[i].Length <= 3)
+        {
+            Console.Write($"{arr[i]} \t");
+        }
+    }
+    Console.WriteLine();
+}
+CreateNewArray(arr);
